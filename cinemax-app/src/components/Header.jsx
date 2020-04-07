79b208297dd634ce18d8b70738776150c96cecd3
AlinaSp <alinasapiannik@gmail.com>
2020-04-08 01:20:32 +0300

    part 1

---
+++ cinemax-app/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Logo } from "./Logo";
+import { Nav } from "./Nav";
+import { UserIcon } from "./User";
+
+
+export const Header = () => (
+  <header>
+    <div className="left-block">
+      <Logo/>
+      <Nav/>
+    </div>
+    <div className="right-block">
+      <UserIcon/>
+    </div>
+  </header>
+);
---
+++ cinemax-app/src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Nav = () => (
+  <div className="nav">
+    nav
+  </div>
+);
